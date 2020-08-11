32d285c5fe382bc5b3802029a3544f99c65c9448
philip <giphilgolden@gmail.com>
2020-08-11 10:30:16 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import { Route,Switch } from 'react-router-dom';
+import Navigation from "./components/navigation/Navigation"
+import FourOhFour from "./components/FourOhFour/FourOhFour"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Welcome name="Davey" />
+      <Clock />
+      <Contact /> */}
+
+        <Navigation />
+
+      <Switch>
+
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Davey" />}
+        />
+
+        <Route
+          exact
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+
+        <Route 
+          exact
+          path="/clock"
+          component={Clock}
+        />
+
+        <Route 
+          exact
+          path="/contact"
+          component={Contact}
+        />
+
+        <Route 
+        component={FourOhFour}
+        />
+
+      </Switch>
+
+
     </div>
+
   );
 }
+
+
 
 export default App;
---
+++ src/components/FourOhFour/FourOhFour.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FourOhFour(props) {
+    return (
+        <div className="FourOhFour">
+            404
+        </div>
+    );
+}
+
+export default FourOhFour
